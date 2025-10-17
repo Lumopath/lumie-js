051a0c2a6e578e5d987db7d681b3d9c362818ed2
Yuta Morinaga <y.c.morinaga+1@gmail.com>
2025-10-17 16:45:48 -0400

    User process.env.NEXT_PUBLIC_CODESPACE_NAME for the API URL

--- web/lib/graphql-client.ts
+++ web/lib/graphql-client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/graphql';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || `https://${process.env.NEXT_PUBLIC_CODESPACE_NAME}-3001.app.github.dev/graphql`;;
 
 export const graphQLClient = new GraphQLClient(API_URL, {
   headers: {
--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_CODESPACE_NAME: process.env.CODESPACE_NAME,
+  },
 }
 
 module.exports = nextConfig
