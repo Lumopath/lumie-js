09217dbd1daf4385082c57b48c35555e16ae94c0
Yuta Morinaga <y.c.morinaga+1@gmail.com>
2025-10-23 17:31:11 -0400

    Update port for API

--- web/lib/graphql-client.ts
+++ web/lib/graphql-client.ts
@@ -1,8 +1,8 @@
 import { GraphQLClient } from 'graphql-request';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL ||
-  process.env.NEXT_PUBLIC_CODESPACE_NAME ? `https://${process.env.NEXT_PUBLIC_CODESPACE_NAME}-3001.app.github.dev/graphql` :
-  `http://localhost:3001/graphql`;
+  process.env.NEXT_PUBLIC_CODESPACE_NAME ? `https://${process.env.NEXT_PUBLIC_CODESPACE_NAME}-5000.app.github.dev/graphql` :
+  `http://localhost:5000/graphql`;
 
 export const graphQLClient = new GraphQLClient(API_URL, {
   headers: {
